148d766bb2858fed0be6b9472e95647d197d8f72
thenotoriouscoders <91683185+thenotoriouscoders@users.noreply.github.com>
2021-10-05 17:55:05 +0530

    Add files via upload

---
+++ assignment- 1/Assignment.java
@@ -0,0 +1,10 @@
+package com.java;
+
+public class Assignment {
+    // Write a Java Program to print your Introduction on Screen?
+
+//        Sout();
+public static void main(String[] args) {
+        System.out.println("Hello! My name is Abhinav Chakravarty");
+}
+}
